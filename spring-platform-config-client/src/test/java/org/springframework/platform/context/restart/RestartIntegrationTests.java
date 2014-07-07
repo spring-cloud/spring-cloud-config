f8497e62c7a2baf4d0739c63ad8bee61498ac0ee
Dave Syer <dsyer@gopivotal.com>
2014-07-07 15:36:55 +0100

    Correct type in port for test

--- spring-platform-config-client/src/test/java/org/springframework/platform/context/restart/RestartIntegrationTests.java
+++ spring-platform-config-client/src/test/java/org/springframework/platform/context/restart/RestartIntegrationTests.java
@@ -41,7 +41,7 @@ public class RestartIntegrationTests {
 	@Test
 	public void testRestartTwice() throws Exception {
 
-		context = SpringApplication.run(TestConfiguration.class, "--endpoints.restart.enabled=true", "--servert.port=0");
+		context = SpringApplication.run(TestConfiguration.class, "--endpoints.restart.enabled=true", "--server.port=0");
 		RestartEndpoint endpoint = context.getBean(RestartEndpoint.class);
 		assertNotNull(context.getParent());
 		assertNull(context.getParent().getParent());
