aa2150deda182647ae2e18639b097591fc522bfd
Dave Syer <dsyer@pivotal.io>
2015-03-20 15:09:09 +0000

    Fix broken test

--- spring-cloud-config-sample/src/test/java/sample/ApplicationFailFastTests.java
+++ spring-cloud-config-sample/src/test/java/sample/ApplicationFailFastTests.java
@@ -8,13 +8,13 @@ import static org.junit.Assert.*;
 public class ApplicationFailFastTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextFails() {
         try {
             new SpringApplicationBuilder()
                     .sources(Application.class)
                     .run("--server.port=0", "--spring.cloud.config.enabled=true", 
                             "--spring.cloud.config.failFast=true",
-                            "--spring.cloud.config.uit=http://server-host-doesnt-exist:1234");
+                            "--spring.cloud.config.uri=http://server-host-doesnt-exist:1234");
             fail("failFast option did not produce an exception");
         } catch (Exception e) {
             assertTrue("Exception not caused by fail fast", e.getMessage().contains("fail fast"));
