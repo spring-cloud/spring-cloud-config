ad8a9c689f9e8c3c79de4e123aea1295b1b2409b
Dave Syer <dsyer@pivotal.io>
2015-10-15 17:13:08 +0000

    Sync docs from master to gh-pages

--- ghpages.sh
+++ ghpages.sh
@@ -12,20 +12,13 @@ if ! [ -d docs/target/generated-docs ]; then
     exit 0
 fi
 
-# Find name of current branch
-###################################################################
-branch=$TRAVIS_BRANCH
-[ "$branch" == "" ] && branch=`git rev-parse --abbrev-ref HEAD`
-target=.
-if [ "$branch" != "master" ]; then target=./$branch; mkdir -p $target; fi
-
 # Stash any outstanding changes
 ###################################################################
 git diff-index --quiet HEAD
 dirty=$?
 if [ "$dirty" != "0" ]; then git stash; fi
 
-# Switch to gh-pages branch to sync it with current branch
+# Switch to gh-pages branch to sync it with master
 ###################################################################
 git checkout gh-pages
 
@@ -33,22 +26,21 @@ for f in docs/target/generated-docs/*; do
     file=${f#docs/target/generated-docs/*}
     if ! git ls-files -i -o --exclude-standard --directory | grep -q ^$file$; then
         # Not ignored...
-        cp -rf $f $target
-        git add -A $target/$file
+        cp -rf $f .
+        git add -A $file
     fi
 done
 
-git add -A README.adoc || echo "No change to README.adoc"
-git commit -a -m "Sync docs from $branch to gh-pages" || echo "Nothing committed"
+git commit -a -m "Sync docs from master to gh-pages"
 
 # Uncomment the following push if you want to auto push to
-# the gh-pages branch whenever you commit to branch locally.
+# the gh-pages branch whenever you commit to master locally.
 # This is a little extreme. Use with care!
 ###################################################################
-git push origin gh-pages || echo "Cannot push gh-pages"
+git push origin gh-pages
 
-# Finally, switch back to the current branch and exit block
-git checkout $branch
+# Finally, switch back to the master branch and exit block
+git checkout master
 if [ "$dirty" != "0" ]; then git stash pop; fi
 
 exit 0
