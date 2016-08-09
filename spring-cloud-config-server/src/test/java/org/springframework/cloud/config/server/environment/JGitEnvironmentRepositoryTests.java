218d140c7e8e329d11d3c7e9a181215b58092cad
Marcos Barbero <marcos.hgb@gmail.com>
2016-08-09 15:42:13 -0300

    Add force-pull flag for dirty repositories
    fixes gh-430

--- spring-cloud-config-server/src/test/java/org/springframework/cloud/config/server/environment/JGitEnvironmentRepositoryTests.java
+++ spring-cloud-config-server/src/test/java/org/springframework/cloud/config/server/environment/JGitEnvironmentRepositoryTests.java
@@ -16,9 +16,28 @@
 
 package org.springframework.cloud.config.server.environment;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.StatusCommand;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.util.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.server.test.ConfigServerTestUtils;
+import org.springframework.core.env.StandardEnvironment;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
@@ -26,18 +45,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import java.io.File;
-import java.io.IOException;
-
-import org.eclipse.jgit.api.CloneCommand;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.util.FileUtils;
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.cloud.config.environment.Environment;
-import org.springframework.cloud.config.server.test.ConfigServerTestUtils;
-import org.springframework.core.env.StandardEnvironment;
 
 /**
  * @author Dave Syer
@@ -225,6 +232,87 @@ public class JGitEnvironmentRepositoryTests {
 		envRepository.afterPropertiesSet();
 		verify(mockCloneCommand, times(0)).call();
 		verify(mockGit, times(0)).fetch();
+	}
+
+	@Test
+	public void shouldPullForcepullNotClean() throws Exception {
+		Git git = mock(Git.class);
+		StatusCommand statusCommand = mock(StatusCommand.class);
+		Status status = mock(Status.class);
+
+		when(git.status()).thenReturn(statusCommand);
+		when(statusCommand.call()).thenReturn(status);
+		when(status.isClean()).thenReturn(false);
+
+		JGitEnvironmentRepository repo = new JGitEnvironmentRepository(
+				this.environment);
+		repo.setForcePull(true);
+
+		boolean shouldPull = repo.shouldPull(git, null);
+
+		assertThat("shouldPull was false", shouldPull, is(true));
+	}
+
+	@Test
+	public void shouldPullForcepullClean() throws Exception {
+		Git git = mock(Git.class);
+		StatusCommand statusCommand = mock(StatusCommand.class);
+		Status status = mock(Status.class);
+
+		when(git.status()).thenReturn(statusCommand);
+		when(statusCommand.call()).thenReturn(status);
+		when(status.isClean()).thenReturn(true);
+
+		JGitEnvironmentRepository repo = new JGitEnvironmentRepository(
+				this.environment);
+		repo.setForcePull(true);
+
+		boolean shouldPull = repo.shouldPull(git, null);
+
+		assertThat("shouldPull was true", shouldPull, is(false));
+	}
+
+	@Test
+	public void shouldPullNotClean() throws Exception {
+		Git git = mock(Git.class);
+		StatusCommand statusCommand = mock(StatusCommand.class);
+		Status status = mock(Status.class);
+		Ref ref = mock(Ref.class);
+
+		when(git.status()).thenReturn(statusCommand);
+		when(statusCommand.call()).thenReturn(status);
+		when(status.isClean()).thenReturn(false);
+
+		JGitEnvironmentRepository repo = new JGitEnvironmentRepository(
+				this.environment);
+
+		boolean shouldPull = repo.shouldPull(git, ref);
+
+		assertThat("shouldPull was true", shouldPull, is(false));
+	}
+
+	@Test
+	public void shouldPullClean() throws Exception {
+		Git git = mock(Git.class);
+		StatusCommand statusCommand = mock(StatusCommand.class);
+		Status status = mock(Status.class);
+		Ref ref = mock(Ref.class);
+		Repository repository = mock(Repository.class);
+		StoredConfig storedConfig = mock(StoredConfig.class);
+
+		when(git.status()).thenReturn(statusCommand);
+		when(git.getRepository()).thenReturn(repository);
+		when(repository.getConfig()).thenReturn(storedConfig);
+		when(storedConfig.getString("remote", "origin", "url")).thenReturn("http://example/git");
+		when(statusCommand.call()).thenReturn(status);
+		when(status.isClean()).thenReturn(true);
+
+		JGitEnvironmentRepository repo = new JGitEnvironmentRepository(
+				this.environment);
+
+		boolean shouldPull = repo.shouldPull(git, ref);
+
+		assertThat("shouldPull was false", shouldPull, is(true));
 	}
 
 	class MockGitFactory extends JGitEnvironmentRepository.JGitFactory {
