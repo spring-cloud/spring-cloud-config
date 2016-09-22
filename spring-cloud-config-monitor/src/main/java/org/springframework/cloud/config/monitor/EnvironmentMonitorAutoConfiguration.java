e51d3f617b705650e99d5a2bb501f88aa8c2812f
Will Boyd <whboyd@liberty.edu>
2016-09-22 15:23:09 -0400

    move PropertyPathNotificationExtractorConfiguration to an inner class of EnvironmentMonitorAutoConfiguration

--- spring-cloud-config-monitor/src/main/java/org/springframework/cloud/config/monitor/EnvironmentMonitorAutoConfiguration.java
+++ spring-cloud-config-monitor/src/main/java/org/springframework/cloud/config/monitor/EnvironmentMonitorAutoConfiguration.java
@@ -27,11 +27,12 @@ import org.springframework.context.annotation.Import;
 
 /**
  * @author Dave Syer
+ * @author Will Boyd
  *
  */
 @Configuration
 @ConditionalOnWebApplication
-@Import({FileMonitorConfiguration.class, PropertyPathNotificationExtractorConfiguration.class})
+@Import(FileMonitorConfiguration.class)
 public class EnvironmentMonitorAutoConfiguration {
 
 	@Autowired(required=false)
@@ -41,5 +42,28 @@ public class EnvironmentMonitorAutoConfiguration {
 	public PropertyPathEndpoint propertyPathEndpoint() {
 		return new PropertyPathEndpoint(new CompositePropertyPathNotificationExtractor(this.extractors));
 	}
+        
+        @Configuration
+        protected static class PropertyPathNotificationExtractorConfiguration {
+            
+                @Bean
+                @ConditionalOnProperty(value="spring.cloud.config.server.monitor.github.enabled", havingValue="true", matchIfMissing=true)
+                public GithubPropertyPathNotificationExtractor githubPropertyPathNotificationExtractor() {
+                        return new GithubPropertyPathNotificationExtractor();
+                }
+
+                @Bean
+                @ConditionalOnProperty(value="spring.cloud.config.server.monitor.gitlab.enabled", havingValue="true", matchIfMissing=true)
+                public GitlabPropertyPathNotificationExtractor gitlabPropertyPathNotificationExtractor() {
+                        return new GitlabPropertyPathNotificationExtractor();
+                }
+
+                @Bean
+                @ConditionalOnProperty(value="spring.cloud.config.server.monitor.bitbucket.enabled", havingValue="true", matchIfMissing=true)
+                public BitbucketPropertyPathNotificationExtractor bitbucketPropertyPathNotificationExtractor() {
+                        return new BitbucketPropertyPathNotificationExtractor();
+                }
+        
+        }
 
 }
