7e72a5063e95c9a0821f9f6c491fd3068f219f88
Mathieu Ouellet <mat_ouel@hotmail.com>
2017-03-02 09:11:41 -0500

    Revert "Custom headers property test"
    This reverts commit 307950222458a373621276dfa49dc15359df2af6.

--- spring-cloud-config-client/src/test/java/org/springframework/cloud/config/client/ConfigServicePropertySourceLocatorTests.java
+++ spring-cloud-config-client/src/test/java/org/springframework/cloud/config/client/ConfigServicePropertySourceLocatorTests.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.assertNull;
 
 import java.io.ByteArrayInputStream;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.hamcrest.core.IsInstanceOf;
 import org.hamcrest.core.IsNull;
@@ -28,7 +26,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.util.Base64Utils;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -137,153 +134,6 @@ public class ConfigServicePropertySourceLocatorTests {
 		assertNull(this.locator.locate(this.environment));
 	}
 
-	@Test
-	public void sunnyDayWithCustomHeaders() throws Exception {
-		ClientHttpRequestFactory requestFactory = Mockito
-				.mock(ClientHttpRequestFactory.class);
-		ClientHttpRequest request = Mockito.mock(ClientHttpRequest.class);
-		Mockito.when(requestFactory.createRequest(Mockito.any(URI.class),
-				Mockito.any(HttpMethod.class))).thenReturn(request);
-
-		RestTemplate restTemplate = Mockito.spy(new RestTemplate(requestFactory));
-
-		Map<String, String> customHeaders = new HashMap<>();
-		customHeaders.put("X-Example-Version", "2.1");
-
-		ConfigClientProperties defaults = new ConfigClientProperties(this.environment);
-		defaults.setHeaders(customHeaders);
-
-		this.locator = new ConfigServicePropertySourceLocator(defaults);
-		this.locator.setRestTemplate(restTemplate);
-		this.locator.locate(this.environment);
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("X-Example-Version", "2.1");
-		HttpEntity<Void> entity = new HttpEntity<>((Void) null, headers);
-
-		Mockito.verify(restTemplate).exchange(Mockito.anyString(),
-				Mockito.any(HttpMethod.class), Mockito.eq(entity),
-				Mockito.eq(Environment.class), Mockito.any(), Mockito.any());
-	}
-
-	@Test
-	public void passwordPropertyAddsAuthorizationHeader() throws Exception {
-		ClientHttpRequestFactory requestFactory = Mockito
-				.mock(ClientHttpRequestFactory.class);
-		ClientHttpRequest request = Mockito.mock(ClientHttpRequest.class);
-		Mockito.when(requestFactory.createRequest(Mockito.any(URI.class),
-				Mockito.any(HttpMethod.class))).thenReturn(request);
-
-		RestTemplate restTemplate = Mockito.spy(new RestTemplate(requestFactory));
-
-		ConfigClientProperties defaults = new ConfigClientProperties(this.environment);
-		defaults.setUsername("username");
-		defaults.setPassword("password");
-
-		this.locator = new ConfigServicePropertySourceLocator(defaults);
-		this.locator.setRestTemplate(restTemplate);
-		this.locator.locate(this.environment);
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Basic "
-				+ encodePassword(defaults.getUsername(), defaults.getPassword()));
-		HttpEntity<Void> entity = new HttpEntity<>((Void) null, headers);
-
-		Mockito.verify(restTemplate).exchange(Mockito.anyString(),
-				Mockito.any(HttpMethod.class), Mockito.eq(entity),
-				Mockito.eq(Environment.class), Mockito.any(), Mockito.any());
-	}
-
-	@Test
-	public void authorizationPropertyAddsGenericAuthorizationHeader() throws Exception {
-		ClientHttpRequestFactory requestFactory = Mockito
-				.mock(ClientHttpRequestFactory.class);
-		ClientHttpRequest request = Mockito.mock(ClientHttpRequest.class);
-		Mockito.when(requestFactory.createRequest(Mockito.any(URI.class),
-				Mockito.any(HttpMethod.class))).thenReturn(request);
-
-		RestTemplate restTemplate = Mockito.spy(new RestTemplate(requestFactory));
-
-		ConfigClientProperties defaults = new ConfigClientProperties(this.environment);
-		defaults.setAuthorization("Basic dXNlcm5hbWU6cGFzc3dvcmQ=");
-
-		this.locator = new ConfigServicePropertySourceLocator(defaults);
-		this.locator.setRestTemplate(restTemplate);
-		this.locator.locate(this.environment);
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Basic dXNlcm5hbWU6cGFzc3dvcmQ=");
-		HttpEntity<Void> entity = new HttpEntity<>((Void) null, headers);
-
-		Mockito.verify(restTemplate).exchange(Mockito.anyString(),
-				Mockito.any(HttpMethod.class), Mockito.eq(entity),
-				Mockito.eq(Environment.class), Mockito.any(), Mockito.any());
-	}
-
-	@Test
-	public void failWhenBothPasswordAndAuthorizationPropertiesSet() throws Exception {
-		ClientHttpRequestFactory requestFactory = Mockito
-				.mock(ClientHttpRequestFactory.class);
-		ClientHttpRequest request = Mockito.mock(ClientHttpRequest.class);
-		Mockito.when(requestFactory.createRequest(Mockito.any(URI.class),
-				Mockito.any(HttpMethod.class))).thenReturn(request);
-
-		RestTemplate restTemplate = Mockito.spy(new RestTemplate(requestFactory));
-
-		ConfigClientProperties defaults = new ConfigClientProperties(this.environment);
-		defaults.setFailFast(true);
-		defaults.setUsername("username");
-		defaults.setPassword("password");
-		defaults.setAuthorization("Basic "
-				+ encodePassword(defaults.getUsername(), defaults.getPassword()));
-
-		this.expected.expectCause(
-				IsInstanceOf.<Throwable> instanceOf(IllegalStateException.class));
-
-		this.locator = new ConfigServicePropertySourceLocator(defaults);
-		this.locator.setRestTemplate(restTemplate);
-		this.locator.locate(this.environment);
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Basic "
-				+ encodePassword(defaults.getUsername(), defaults.getPassword()));
-		HttpEntity<Void> entity = new HttpEntity<>((Void) null, headers);
-
-		Mockito.verify(restTemplate).exchange(Mockito.anyString(),
-				Mockito.any(HttpMethod.class), Mockito.eq(entity),
-				Mockito.eq(Environment.class), Mockito.any(), Mockito.any());
-	}
-
-	@Test
-	public void authorizationHeaderTakesPrecedenceOverProperties() throws Exception {
-		ClientHttpRequestFactory requestFactory = Mockito
-				.mock(ClientHttpRequestFactory.class);
-		ClientHttpRequest request = Mockito.mock(ClientHttpRequest.class);
-		Mockito.when(requestFactory.createRequest(Mockito.any(URI.class),
-				Mockito.any(HttpMethod.class))).thenReturn(request);
-
-		RestTemplate restTemplate = Mockito.spy(new RestTemplate(requestFactory));
-
-		Map<String, String> customHeaders = new HashMap<>();
-		customHeaders.put("Authorization", "Custom Headers");
-
-		ConfigClientProperties defaults = new ConfigClientProperties(this.environment);
-		defaults.setAuthorization("Basic dXNlcm5hbWU6cGFzc3dvcmQ=");
-		defaults.setHeaders(customHeaders);
-
-		this.locator = new ConfigServicePropertySourceLocator(defaults);
-		this.locator.setRestTemplate(restTemplate);
-		this.locator.locate(this.environment);
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Custom Headers");
-		HttpEntity<Void> entity = new HttpEntity<>((Void) null, headers);
-
-		Mockito.verify(restTemplate).exchange(Mockito.anyString(),
-				Mockito.any(HttpMethod.class), Mockito.eq(entity),
-				Mockito.eq(Environment.class), Mockito.any(), Mockito.any());
-	}
-
 	@SuppressWarnings("unchecked")
 	private void mockRequestResponseWithLabel(ResponseEntity<?> response, String label) {
 		Mockito.when(
@@ -301,9 +151,4 @@ public class ConfigServicePropertySourceLocatorTests {
 						Mockito.any(Class.class), Matchers.anyString(),
 						Matchers.anyString())).thenReturn(response);
 	}
-
-	private String encodePassword(String username, String password) {
-		return new String(Base64Utils.encode((username + ":" + password).getBytes()));
-	}
-
 }
