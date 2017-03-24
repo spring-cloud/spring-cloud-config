3abc1ab84c4e264a99db0d55b18abe1f873f0f7f
Nastya Smirnova <zminyty@gmail.com>
2017-03-24 22:14:51 +0200

    Makes fetching of config server from Eureka retryable
    Makes fetching of config server from Eureka retryable, adds support for failfast
    
    Retry requires spring-retry to be on the classpath.

---
+++ spring-cloud-config-client/src/test/java/org/springframework/cloud/config/client/BaseDiscoveryClientConfigServiceBootstrapConfigurationTests.java
@@ -0,0 +1,106 @@
+package org.springframework.cloud.config.client;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
+import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.test.util.EnvironmentTestUtils;
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.commons.util.UtilAutoConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.cloud.config.client.ConfigClientProperties.Discovery.DEFAULT_CONFIG_SERVER;
+
+public abstract class BaseDiscoveryClientConfigServiceBootstrapConfigurationTests {
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	protected AnnotationConfigApplicationContext context;
+
+	protected DiscoveryClient client = Mockito.mock(DiscoveryClient.class);
+
+	protected ServiceInstance info = new DefaultServiceInstance("app", "foo", 8877,
+			false);
+
+	@After
+	public void close() {
+		if (this.context != null) {
+			this.context.close();
+		}
+	}
+
+	void givenDiscoveryClientReturnsNoInfo() {
+		given(this.client.getInstances(DEFAULT_CONFIG_SERVER))
+				.willReturn(Collections.<ServiceInstance> emptyList());
+	}
+
+	void givenDiscoveryClientReturnsInfo() {
+		given(this.client.getInstances(DEFAULT_CONFIG_SERVER))
+				.willReturn(Arrays.asList(this.info));
+	}
+
+	void givenDiscoveryClientReturnsInfoOnThirdTry() {
+		given(this.client.getInstances(DEFAULT_CONFIG_SERVER))
+				.willReturn(Collections.<ServiceInstance> emptyList())
+				.willReturn(Collections.<ServiceInstance> emptyList())
+				.willReturn(Arrays.asList(this.info));
+	}
+
+	void expectNoInstancesOfConfigServerException() {
+		expectedException.expect(IllegalStateException.class);
+		expectedException.expectMessage(
+				"No instances found of configserver (" + DEFAULT_CONFIG_SERVER + ")");
+	}
+
+	void expectDiscoveryClientConfigServiceBootstrapConfigurationIsSetup() {
+		assertEquals(1, this.context.getBeanNamesForType(
+				DiscoveryClientConfigServiceBootstrapConfiguration.class).length);
+	}
+
+	void expectConfigClientPropertiesHasDefaultConfiguration() {
+		expectConfigClientPropertiesHasConfiguration("http://localhost:8888");
+	}
+
+	void expectConfigClientPropertiesHasConfigurationFromEureka() {
+		expectConfigClientPropertiesHasConfiguration("http://foo:8877/");
+	}
+
+	void expectConfigClientPropertiesHasConfiguration(final String expectedUri) {
+		ConfigClientProperties properties = this.context
+				.getBean(ConfigClientProperties.class);
+		assertEquals(expectedUri, properties.getRawUri());
+	}
+
+	void verifyDiscoveryClientCalledThreeTimes() {
+		verify(this.client, times(3)).getInstances(DEFAULT_CONFIG_SERVER);
+	}
+
+	void verifyDiscoveryClientCalledOnce() {
+		verify(this.client).getInstances(DEFAULT_CONFIG_SERVER);
+	}
+
+	void setup(String... env) {
+		this.context = new AnnotationConfigApplicationContext();
+		EnvironmentTestUtils.addEnvironment(this.context, env);
+		EnvironmentTestUtils.addEnvironment(this.context, "eureka.client.enabled=false");
+		this.context.getDefaultListableBeanFactory().registerSingleton("discoveryClient",
+				this.client);
+		this.context.register(UtilAutoConfiguration.class,
+				PropertyPlaceholderAutoConfiguration.class,
+				DiscoveryClientConfigServiceBootstrapConfiguration.class,
+				ConfigServiceBootstrapConfiguration.class, ConfigClientProperties.class);
+		this.context.refresh();
+	}
+
+}
