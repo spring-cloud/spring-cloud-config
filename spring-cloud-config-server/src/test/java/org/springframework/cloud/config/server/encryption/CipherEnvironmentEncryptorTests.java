e380b83026b3737be7b566cbc88be6dce5e20e3d
ramaiahkk <ramaiahkk@gmail.com>
2018-08-06 12:51:43 -0400

    #1103 java.lang.NullPointerException (#1104)
    java.lang.NullPointerException while decrypting due to value is null and still looking for cipher string. #1103

--- spring-cloud-config-server/src/test/java/org/springframework/cloud/config/server/encryption/CipherEnvironmentEncryptorTests.java
+++ spring-cloud-config-server/src/test/java/org/springframework/cloud/config/server/encryption/CipherEnvironmentEncryptorTests.java
@@ -90,5 +90,18 @@ public class CipherEnvironmentEncryptorTests {
 		assertEquals(secret, this.encryptor.decrypt(environment).getPropertySources()
 				.get(0).getSource().get(environment.getName()));
 	}
+	@Test
+	public void shouldBeAbleToUseNullAsPropertyValue() {
+
+		// when
+		Environment environment = new Environment("name", "profile", "label");
+		environment.add(new PropertySource("a",
+				Collections.<Object, Object>singletonMap(environment.getName(),
+						null)));
+
+		// then
+		assertEquals(null, this.encryptor.decrypt(environment).getPropertySources()
+				.get(0).getSource().get(environment.getName()));
+	}
 
 }
