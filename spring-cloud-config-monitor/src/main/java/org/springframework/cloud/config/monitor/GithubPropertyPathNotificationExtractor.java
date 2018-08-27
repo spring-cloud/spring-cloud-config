861b1a3cd4d60a1c2f140e1db4e6ac048589c958
Juan Pablo Santos Rodríguez <jpsantos@sanitas.es>
2018-08-27 14:37:55 +0200

    GithubPropertyPathNotificationExtractor and GitlabPropertyPathNotificationExtractor using new common base class

--- spring-cloud-config-monitor/src/main/java/org/springframework/cloud/config/monitor/GithubPropertyPathNotificationExtractor.java
+++ spring-cloud-config-monitor/src/main/java/org/springframework/cloud/config/monitor/GithubPropertyPathNotificationExtractor.java
@@ -16,11 +16,6 @@
 
 package org.springframework.cloud.config.monitor;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.util.MultiValueMap;
@@ -31,36 +26,10 @@ import org.springframework.util.MultiValueMap;
  */
 @Order(Ordered.LOWEST_PRECEDENCE - 300)
 public class GithubPropertyPathNotificationExtractor
-		implements PropertyPathNotificationExtractor {
+		extends BasePropertyPathNotificationExtractor {
 
 	@Override
-	public PropertyPathNotification extract(MultiValueMap<String, String> headers,
-			Map<String, Object> request) {
-		if ("push".equals(headers.getFirst("X-Github-Event"))) {
-			if (request.get("commits") instanceof Collection) {
-				Set<String> paths = new HashSet<>();
-				@SuppressWarnings("unchecked")
-				Collection<Map<String, Object>> commits = (Collection<Map<String, Object>>) request
-						.get("commits");
-				for (Map<String, Object> commit : commits) {
-					addAllPaths(paths, commit, "added");
-					addAllPaths(paths, commit, "removed");
-					addAllPaths(paths, commit, "modified");
-				}
-				if (!paths.isEmpty()) {
-					return new PropertyPathNotification(paths.toArray(new String[0]));
-				}
-			}
-		}
-		return null;
-	}
-
-	private void addAllPaths(Set<String> paths, Map<String, Object> commit, String name) {
-		@SuppressWarnings("unchecked")
-		Collection<String> files = (Collection<String>) commit.get(name);
-		if (files != null) {
-			paths.addAll(files);
-		}
-	}
-
+    protected boolean requestBelongsToGitRepoManager(MultiValueMap<String, String> headers) {
+        return "push".equals(headers.getFirst("X-Github-Event"));
+    }
 }
