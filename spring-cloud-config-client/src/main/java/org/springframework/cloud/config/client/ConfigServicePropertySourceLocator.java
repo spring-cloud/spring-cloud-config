72824ba5d580dbd50e7cf47259a8f714426cc629
Spencer Gibb <spencer@gibb.us>
2019-12-17 17:21:25 -0500

    Makes locateCollection() retryable

--- spring-cloud-config-client/src/main/java/org/springframework/cloud/config/client/ConfigServicePropertySourceLocator.java
+++ spring-cloud-config-client/src/main/java/org/springframework/cloud/config/client/ConfigServicePropertySourceLocator.java
@@ -18,6 +18,7 @@ package org.springframework.cloud.config.client;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -144,6 +145,13 @@ public class ConfigServicePropertySourceLocator implements PropertySourceLocator
 				? error == null ? "label not found" : error.getMessage() : errorBody));
 		return null;
 
+	}
+
+	@Override
+	@Retryable(interceptor = "configServerRetryInterceptor")
+	public Collection<org.springframework.core.env.PropertySource<?>> locateCollection(
+			org.springframework.core.env.Environment environment) {
+		return PropertySourceLocator.locateCollection(this, environment);
 	}
 
 	private void log(Environment result) {
