fa4a6a2a60bf6a7e17a83172ee5fd857023c3930
spencergibb <spencer@gibb.us>
2020-07-24 19:36:05 -0400

    Initial support for new ConfigDataLoader infrastructure.
    This allows for spring.config.import=configserver:

---
+++ spring-cloud-config-client/src/test/java/org/springframework/cloud/config/client/ConfigServerConfigDataLocationResolverTests.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2013-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.config.client;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.context.config.ConfigDataLocationResolverContext;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.boot.logging.DeferredLog;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class ConfigServerConfigDataLocationResolverTests {
+
+	private ConfigServerConfigDataLocationResolver resolver;
+
+	private ConfigDataLocationResolverContext context = mock(
+			ConfigDataLocationResolverContext.class);
+
+	private MockEnvironment environment;
+
+	private Binder environmentBinder;
+
+	@BeforeEach
+	void setup() {
+		this.environment = new MockEnvironment();
+		this.environmentBinder = Binder.get(this.environment);
+		this.resolver = new ConfigServerConfigDataLocationResolver(new DeferredLog());
+	}
+
+	@Test
+	void isResolvableReturnsFalseWithIncorrectPrefix() {
+		assertThat(this.resolver.isResolvable(this.context, "test:")).isFalse();
+	}
+
+	@Test
+	void isResolvableReturnsTrueWithCorrectPrefix() {
+		assertThat(this.resolver.isResolvable(this.context, "configserver:")).isTrue();
+	}
+
+}
