95f22177f29c63bd4745b66b1977d660bd003834
spencergibb <spencer@gibb.us>
2020-08-31 19:21:00 -0400

    Removes renamed property

--- spring-cloud-config-server/src/main/java/org/springframework/cloud/config/server/environment/NativeEnvironmentRepository.java
+++ spring-cloud-config-server/src/main/java/org/springframework/cloud/config/server/environment/NativeEnvironmentRepository.java
@@ -220,8 +220,6 @@ public class NativeEnvironmentRepository
 		map.put("spring.config.location", StringUtils.arrayToCommaDelimitedString(
 				getLocations(application, profile, label).getLocations()));
 		// globally ignore config files that are not found
-		map.put("spring.config.all-locations-optional", true); // TODO: remove old
-																// property
 		map.put("spring.config.on-location-not-found", "ignore");
 		environment.getPropertySources()
 				.addFirst(new MapPropertySource("config-data-setup", map));
