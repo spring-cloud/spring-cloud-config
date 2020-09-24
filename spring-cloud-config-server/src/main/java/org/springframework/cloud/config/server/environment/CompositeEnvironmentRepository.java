158d04f4843a3dad6b77f730b1a1070c1430eddc
Craig Walls <cwalls@pivotal.io>
2020-09-24 06:55:11 -0600

    Fix checkstyle unused import error

--- spring-cloud-config-server/src/main/java/org/springframework/cloud/config/server/environment/CompositeEnvironmentRepository.java
+++ spring-cloud-config-server/src/main/java/org/springframework/cloud/config/server/environment/CompositeEnvironmentRepository.java
@@ -23,7 +23,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.cloud.config.environment.Environment;
-import org.springframework.cloud.config.environment.PropertySource;
 import org.springframework.core.OrderComparator;
 
 /**
