66c0715d62b18d4f5f62a0359489ea5cac647afa
buildmaster <builds@springframework.org>
2023-10-19 10:36:53 +0000

    Bumping versions

--- spring-cloud-config-server/src/main/java/org/springframework/cloud/config/server/encryption/EncryptionController.java
+++ spring-cloud-config-server/src/main/java/org/springframework/cloud/config/server/encryption/EncryptionController.java
@@ -136,8 +136,8 @@ public class EncryptionController {
 		}
 		catch (IllegalArgumentException | IllegalStateException e) {
 			if (logger.isErrorEnabled()) {
-				logger.error("Cannot decrypt key:" + name + ", value:" + data +
-					", Please verify if encrypt.key is set correctly", e);
+				logger.error("Cannot decrypt key:" + name + ", value:" + data
+						+ ", Please verify if encrypt.key is set correctly", e);
 			}
 			throw new InvalidCipherException();
 		}
