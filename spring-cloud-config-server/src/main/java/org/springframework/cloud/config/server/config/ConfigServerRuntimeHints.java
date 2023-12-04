886a97cc929361a747336886e486eb9c58170ecd
Olga MaciaszekSharma <omaciaszeksh@vmware.com>
2023-12-04 20:43:52 +0100

    Add more hints. Remove image.properties.

--- spring-cloud-config-server/src/main/java/org/springframework/cloud/config/server/config/ConfigServerRuntimeHints.java
+++ spring-cloud-config-server/src/main/java/org/springframework/cloud/config/server/config/ConfigServerRuntimeHints.java
@@ -16,12 +16,31 @@
 
 package org.springframework.cloud.config.server.config;
 
+import java.security.KeyFactory;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.Signature;
 import java.util.Set;
+
+import javax.crypto.KeyAgreement;
+import javax.crypto.Mac;
+
+import org.apache.sshd.common.channel.ChannelListener;
+import org.apache.sshd.common.forward.PortForwardingEventListener;
+import org.apache.sshd.common.io.nio2.Nio2ServiceFactory;
+import org.apache.sshd.common.io.nio2.Nio2ServiceFactoryFactory;
+import org.apache.sshd.common.session.SessionListener;
+import org.apache.sshd.common.util.security.bouncycastle.BouncyCastleSecurityProviderRegistrar;
+import org.apache.sshd.common.util.security.eddsa.EdDSASecurityProviderRegistrar;
+import org.eclipse.jgit.api.FetchCommand;
+import org.eclipse.jgit.api.MergeCommand;
+import org.eclipse.jgit.internal.transport.sshd.SshdText;
 
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.TypeReference;
+import org.springframework.cloud.config.environment.PropertyValueDescriptor;
 import org.springframework.cloud.config.server.ssh.HostKeyAlgoSupportedValidator;
 import org.springframework.cloud.config.server.ssh.HostKeyAndAlgoBothExistValidator;
 import org.springframework.cloud.config.server.ssh.KnownHostsFileValidator;
@@ -46,9 +65,36 @@ class ConfigServerRuntimeHints implements RuntimeHintsRegistrar {
 		}
 		hints.reflection().registerTypes(Set.of(TypeReference.of(HostKeyAndAlgoBothExistValidator.class),
 				TypeReference.of(KnownHostsFileValidator.class), TypeReference.of(HostKeyAlgoSupportedValidator.class),
-				TypeReference.of(PrivateKeyValidator.class), TypeReference.of(SshPropertyValidator.class)),
-				// TypeReference.of(PropertyValueDescriptor.class)),
+				TypeReference.of(PrivateKeyValidator.class), TypeReference.of(SshPropertyValidator.class),
+				TypeReference.of(PropertyValueDescriptor.class)),
 				hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
+		hints.reflection().registerTypes(
+			Set.of(TypeReference.of(PropertyValueDescriptor.class), TypeReference.of(Mac.class),
+				TypeReference.of(KeyAgreement.class), TypeReference.of(KeyPairGenerator.class),
+				TypeReference.of(KeyFactory.class), TypeReference.of(Signature.class),
+				TypeReference.of(MessageDigest.class)),
+			hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
+		if (ClassUtils.isPresent("org.apache.sshd.common.SshConstants", classLoader)) {
+			hints.reflection()
+				.registerTypes(Set.of(TypeReference.of(BouncyCastleSecurityProviderRegistrar.class),
+						TypeReference.of(EdDSASecurityProviderRegistrar.class), TypeReference.of(SshdText.class),
+						TypeReference.of(Nio2ServiceFactory.class), TypeReference.of(Nio2ServiceFactoryFactory.class)),
+					hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
+			hints.reflection()
+				.registerTypes(Set.of(TypeReference.of(MergeCommand.FastForwardMode.Merge.class),
+						TypeReference.of(MergeCommand.ConflictStyle.class),
+						TypeReference.of(MergeCommand.FastForwardMode.class), TypeReference.of(FetchCommand.class)),
+					hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
+			hints.reflection()
+				.registerTypes(Set.of(TypeReference.of(PortForwardingEventListener.class)),
+					hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+						MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.DECLARED_FIELDS));
+			hints.reflection()
+				.registerTypes(Set.of(TypeReference.of(SshdText.class)), hint -> hint
+					.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.DECLARED_FIELDS));
+			hints.proxies().registerJdkProxy(TypeReference.of(ChannelListener.class),
+				TypeReference.of(PortForwardingEventListener.class), TypeReference.of(SessionListener.class));
+		}
 	}
 
 }
